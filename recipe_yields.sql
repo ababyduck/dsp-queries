47b9d1435a19ee94a69c172668f671eb48c0a351
ababyduck <kassisaf@gmail.com>
2019-04-12 14:46:22 -0700

    Improved readability

--- recipe_yields.sql
+++ recipe_yields.sql
@@ -3,19 +3,19 @@ SELECT
 	
 	sr.result AS NQ_id,
 	i0.name AS NQ_name,
-	sr.resultqty AS NQqty,
+	CONCAT ('x', sr.resultqty) AS NQ_qty,
 	
 	sr.resulthq1 AS HQ1_id,
 	i1.name AS HQ1_name,
-	sr.resulthq1qty AS HQ1qty,
+	CONCAT ('x', sr.resulthq1qty) AS HQ1_qty,
 	
 	sr.resulthq2 AS HQ2_id,
 	i2.name AS HQ2_name,
-	sr.resulthq2qty AS HQ2qty,
+	CONCAT ('x', sr.resulthq2qty) AS HQ2_qty,
 	
 	sr.resulthq3 AS HQ3_id,
 	i3.name AS HQ3_name,
-	sr.resulthq3qty AS HQ3qty
+	CONCAT ('x', sr.resulthq3qty) AS HQ3_qty
 	
 FROM synth_recipes sr
 JOIN item_basic AS i0 ON sr.result = i0.itemid
