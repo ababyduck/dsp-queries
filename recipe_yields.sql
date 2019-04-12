d03dd57df07945c58680a9791bf8a3504fc38029
ababyduck <kassisaf@gmail.com>
2019-04-12 14:18:59 -0700

    Added quantity to recipe yield query

--- recipe_yields.sql
+++ recipe_yields.sql
@@ -1,13 +1,22 @@
 SELECT
 	sr.id AS synth_id,
-	sr.result AS nq_id,
-	i0.name AS nq_name,
-	sr.resulthq1 AS hq1_id,
-	i1.name AS hq1_name,
-	sr.resulthq2 AS hq2_id,
-	i2.name AS hq2_name,
-	sr.resulthq3 AS hq3_id,
-	i3.name AS hq3_name
+	
+	sr.result AS NQ_id,
+	i0.name AS NQ_name,
+	sr.resultqty AS NQqty,
+	
+	sr.resulthq1 AS HQ1_id,
+	i1.name AS HQ1_name,
+	sr.resulthq1qty AS HQ1qty,
+	
+	sr.resulthq2 AS HQ2_id,
+	i2.name AS HQ2_name,
+	sr.resulthq2qty AS HQ2qty,
+	
+	sr.resulthq3 AS HQ3_id,
+	i3.name AS HQ3_name,
+	sr.resulthq3qty AS HQ3qty
+	
 FROM synth_recipes sr
 JOIN item_basic AS i0 ON sr.result = i0.itemid
 JOIN item_basic AS i1 ON sr.resulthq1 = i1.itemid
