c89fb9479b5530d7cb9d69da375c7e2f1e320776
ababyduck <kassisaf@gmail.com>
2019-06-17 13:45:27 -0700

    Improve output of droprate lookup

--- what_drops_this.sql
+++ what_drops_this.sql
@@ -1,8 +1,10 @@
-SELECT ms.mobid AS "Mob ID",
-    ms.mobname AS "Mob Name",
-    dml.itemRate AS "Drop Rate"
+SELECT (ms.mobid >> 12) & 4095 AS "Zone ID",
+	ms.mobid AS "Mob ID", 
+	ms.mobname AS "Mob Name",
+	ib.NAME AS "Item",
+	dml.itemRate / 10 AS "Drop Rate (%)"
 FROM mob_spawn_points ms
 JOIN mob_groups mg ON mg.groupid = ms.groupid
 JOIN mob_droplist dml ON dml.dropId = mg.dropid
 JOIN item_basic ib ON ib.itemid = dml.itemId
-WHERE ib.name LIKE "mermaid%body"
+WHERE ib.sortname LIKE "mermaid%body"
